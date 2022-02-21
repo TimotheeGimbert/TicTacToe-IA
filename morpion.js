8a1bad2de35b3481662626abac011f661a8a9da8
TimotheeGimbert <timothee.gimbert@free.fr>
2022-02-21 18:28:39 +0100

    adds difficulty selection

--- morpion.js
+++ morpion.js
@@ -11,6 +11,7 @@ class Morpion {
 	];
 
   log = [];
+  difficulty = 'hard';
 
 	constructor(firstPlayer = 'J1') {
 		this.humanPlayer = firstPlayer;
@@ -27,21 +28,24 @@ class Morpion {
 
 		this.gridMap.forEach((line, y) => {
 			line.forEach((cell, x) => {
-				this.getCell(x, y).onclick = () => {
-					this.doPlayHuman(x, y);
-				};
+				this.getCell(x, y).onclick = () => this.doPlayHuman(x, y);
 			});
 		});
+		if (this.iaPlayer === 'J1') this.doPlayIa();
 
-		if (this.iaPlayer === 'J1') {
-			this.doPlayIa();
-		}
-
+    document.getElementById('difficulty').innerHTML = this.difficulty;
     document.getElementById('replayBtn').addEventListener('click', () => this.replay());
     document.getElementById('undoBtn').addEventListener('click', () => this.undo());
     document.getElementById('redoBtn').addEventListener('click', () => this.redo());
-    document.getElementById('logBtn').addEventListener('click', () => console.log(this.log, this.gridMap));
+    //document.getElementById('logBtn').addEventListener('click', () => console.log(this.log, this.gridMap));
+    document.getElementById('difficultyBtn').addEventListener('click', () => this.changeDifficulty());
 	}
+
+  changeDifficulty = () => {
+    this.difficulty = this.difficulty === 'hard' ? 'easy' : 'hard';
+    document.getElementById('difficulty').innerHTML = this.difficulty;
+    console.log(this.difficulty)
+  }
 
   replay = () => {
     localStorage.removeItem('game log');
@@ -50,9 +54,6 @@ class Morpion {
       [null, null, null],
       [null, null, null],
     ];
-    this.initGame();
-    
-    //this.saveLogInStorage();
   }
 
 	getCell = (x, y) => {
@@ -63,62 +64,46 @@ class Morpion {
 	}
 
 	drawHit = (x, y, player) => {
-		if (this.gridMap[y][x] !== null) {
-			return false;
-		}
-
-		this.gridMap[y][x] = player;
+		if (this.gridMap[y][x] !== null) return false;
+    this.gridMap[y][x] = player;
     this.turn += 1;
 		this.getCell(x, y).classList.add(`filled-${player}`);
-    
     this.saveInLog(x, y , player);
-    console.log(`${player} has just played`);
-
 		this.checkWinner(player);
 		return true;
 	}
 
-  saveInLog = (x, y , player) => {
-    this.log.push( { 
-      player: player,
-      x: x,
-      y: y
-    });
-  }
-
+  saveInLog = (x, y , player) => this.log.push( { player: player, x: x, y: y  } );
   saveLogInStorage = () => localStorage.setItem('game log', JSON.stringify(this.log));
 
-  undoHit = () => {
-    const lastHit = this.log.splice(-1)[0];
-    if (lastHit === undefined) return;
-
-    const x = lastHit['x'];
-    const y = lastHit['y'];
-    const player = lastHit['player'];
-    this.getCell(x, y).classList.remove(`filled-${player}`);
-    this.gridMap[y][x] = null;
-    this.turn -= 1;
-  }
-
   undo = () => {
-    this.undoHit();
-    this.undoHit();
-}
-
-  redoHit = () => {
-    const storedLog = JSON.parse(localStorage.getItem('game log'));
-    if (storedLog === null) return;
-    const nextHit = storedLog[this.log.length];
-    if (nextHit === undefined) return;
-    const x = nextHit['x'];
-    const y = nextHit['y'];
-    const player = nextHit['player'];
-    this.drawHit(x, y, player);
+    const undoHit = () => {
+      const lastHit = this.log.splice(-1)[0];
+      if (lastHit === undefined) return;
+      const x = lastHit['x'];
+      const y = lastHit['y'];
+      const player = lastHit['player'];
+      this.getCell(x, y).classList.remove(`filled-${player}`);
+      this.gridMap[y][x] = null;
+      this.turn -= 1;
+    }
+    undoHit();
+    undoHit();
   }
 
   redo = () => {
-    this.redoHit();
-    this.redoHit();
+    const redoHit = () => {
+      const storedLog = JSON.parse(localStorage.getItem('game log'));
+      if (storedLog === null) return;
+      const nextHit = storedLog[this.log.length];
+      if (nextHit === undefined) return;
+      const x = nextHit['x'];
+      const y = nextHit['y'];
+      const player = nextHit['player'];
+      this.drawHit(x, y, player);
+    }
+    redoHit();
+    redoHit();
   }
 
 	doPlayHuman = (x, y) => {
@@ -133,11 +118,17 @@ class Morpion {
 	}
 
 	doPlayIa = () => {
-		if (this.gameOver) {
-			return;
-		}
-
-    const { x, y } = this.minmax(this.gridMap, 0, -Infinity, Infinity, true);
+		if (this.gameOver) return;
+    const getStrat = () => {
+      switch(this.difficulty) {
+        case 'hard':
+          return this.minmaxStrat(this.gridMap, 0, -Infinity, Infinity, true);
+        case 'easy':
+          return this.randomStrat(this.gridMap);
+      }
+    }
+    
+    const {x, y} = getStrat();
     this.drawHit(x, y, this.iaPlayer);
     this.saveLogInStorage();
 	}
@@ -181,33 +172,41 @@ class Morpion {
 }
 
   checkWinner = (lastPlayer) => {
-    const winner = this.getBoardWinner(this.gridMap);
-    if (!winner) {
-        return;
+    const displayEndMessage = (message) => {
+      const endMessageElement = document.getElementById('end-message');
+      endMessageElement.textContent = message;
+      endMessageElement.style.display = 'block';
     }
+    const winner = this.getBoardWinner(this.gridMap);
+    if (!winner) return;
   
     this.gameOver = true;
-    localStorage.removeItem('game log');
     switch(winner) {
       case 'tie':
-        this.displayEndMessage("Vous êtes à égalité !");
+        displayEndMessage("Vous êtes à égalité !");
         break;
       case this.iaPlayer:
-        this.displayEndMessage("L'IA a gagné !");
+        displayEndMessage("L'IA a gagné !");
         break;
       case this.humanPlayer:
-        this.displayEndMessage("Tu as battu l'IA !");
+        displayEndMessage("Tu as battu l'IA !");
         break;
     }
   }
 
-  displayEndMessage = (message) => {
-    const endMessageElement = document.getElementById('end-message');
-    endMessageElement.textContent = message;
-    endMessageElement.style.display = 'block';
+  randomStrat = (board) => {
+    let possibleHits = [];
+    board.forEach( (line, y) => {
+      line.forEach( (cell, x) => {
+        if (cell === null) possibleHits.push({x: x, y: y})
+      });
+    });
+    const randomPosition = Math.floor(Math.random() * possibleHits.length);
+    const randomHit = possibleHits[randomPosition];
+    return randomHit;
   }
 
-  minmax = (board, depth, alpha, beta, isMaximizing) => {
+  minmaxStrat = (board, depth, alpha, beta, isMaximizing) => {
     // Return a score when there is a winner
     const winner = this.getBoardWinner(board);
     if (winner === this.iaPlayer) {
@@ -220,63 +219,63 @@ class Morpion {
         return 0;
     }
     const getSimulatedScore = (x, y, player) => {
-        board[y][x] = player;
-        this.turn += 1;
-        const score = this.minmax(
-            board,
-            depth + 1,
-            alpha,
-            beta,
-            player === this.humanPlayer
-        );
-        board[y][x] = null;
-        this.turn -= 1;
-        return score;
+      board[y][x] = player;
+      this.turn += 1;
+      const score = this.minmaxStrat(
+          board,
+          depth + 1,
+          alpha,
+          beta,
+          player === this.humanPlayer
+      );
+      board[y][x] = null;
+      this.turn -= 1;
+      return score;
     };
     // This tree is going to test every move still possible in game
     // and suppose that the 2 players will always play there best move.
     // The IA search for its best move by testing every combinations,
     // and affects score to every node of the tree.
     if (isMaximizing) {
-        // The higher is the score, the better is the move for the IA.
-        let bestIaScore = -Infinity;
-        let optimalMove;
-        for (const y of [0, 1, 2]) {
-            for (const x of [0, 1, 2]) {
-                if (board[y][x]) {
-                    continue;
-                }
-                const score = getSimulatedScore(x, y, this.iaPlayer);
-                if (score > bestIaScore) {
-                    bestIaScore = score;
-                    optimalMove = { x, y };
-                }
-                // clear useless branch of the algorithm tree
-                // (optional but recommended)
-                alpha = Math.max(alpha, score);
-                if (beta <= alpha) {
-                    break;
-                }
-            }
+      // The higher is the score, the better is the move for the IA.
+      let bestIaScore = -Infinity;
+      let optimalMove;
+      for (const y of [0, 1, 2]) {
+        for (const x of [0, 1, 2]) {
+          if (board[y][x]) {
+              continue;
+          }
+          const score = getSimulatedScore(x, y, this.iaPlayer);
+          if (score > bestIaScore) {
+              bestIaScore = score;
+              optimalMove = { x, y };
+          }
+          // clear useless branch of the algorithm tree
+          // (optional but recommended)
+          alpha = Math.max(alpha, score);
+          if (beta <= alpha) {
+              break;
+          }
         }
-        return (depth === 0) ? optimalMove : bestIaScore;
+      }
+      return (depth === 0) ? optimalMove : bestIaScore;
     }
     // The lower is the score, the better is the move for the player.
     let bestHumanScore = Infinity;
     for (const y of [0, 1, 2]) {
-        for (const x of [0, 1, 2]) {
-            if (board[y][x]) {
-                continue;
-            }
-            const score = getSimulatedScore(x, y, this.humanPlayer);
-            bestHumanScore = Math.min(bestHumanScore, score);
-            // clear useless branch of the algorithm tree
-            // (optional but recommended)
-            beta = Math.min(beta, score);
-            if (beta <= alpha) {
-                break;
-            }
+      for (const x of [0, 1, 2]) {
+        if (board[y][x]) {
+            continue;
         }
+        const score = getSimulatedScore(x, y, this.humanPlayer);
+        bestHumanScore = Math.min(bestHumanScore, score);
+        // clear useless branch of the algorithm tree
+        // (optional but recommended)
+        beta = Math.min(beta, score);
+        if (beta <= alpha) {
+            break;
+        }
+      }
     }
     return bestHumanScore;
   }
